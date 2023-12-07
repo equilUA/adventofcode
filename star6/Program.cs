2a0abc8fd4587d6a5f03641efd53c1f9b7a275a0
Kirill <kyrylo.golovan@protractor.com>
2023-12-07 02:56:42 -0500

    star 6 /7

---
+++ star6/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace star6
+{
+    class Program
+    {
+        internal struct Number
+        {
+            public int Time { get; set; }
+            public int Dist { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Star6!");
+            using (var reader = new StreamReader(@"C:\Stars\adventofcode\star6\input.txt"))
+            {
+                int result = Program.Part2(reader);
+                Console.WriteLine(result);
+            }
+        }
+        static int Part2(StreamReader reader)
+        {
+            string timestr = reader.ReadLine();
+            var time = long.Parse(timestr.Split(':')[1].Replace(" ", ""));
+            Console.WriteLine(time);
+            string distanceStr = reader.ReadLine();
+            var distance = long.Parse(distanceStr.Split(':')[1].Replace(" ", ""));
+            Console.WriteLine(distance);
+
+            var total = 0;
+            for (var j = 0; j < time; ++j)
+            {
+                if (j * (time - j) > distance)
+                {
+                    ++total;
+                }
+            }
+
+            return total;
+        }
+        static int Part1(StreamReader reader)
+        {
+            int total = 1;
+            var numbers = new List<Number>();
+            string time = reader.ReadLine();
+            Console.WriteLine(time);
+            var reveals = time.Split(": ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)[1];
+            var numberRegex = new Regex(@"\d+");
+            var inputTime = numberRegex.Matches(reveals).Select(match => match.Value).ToList();
+
+            string distance = reader.ReadLine();
+            Console.WriteLine(distance);
+            var distanceStr = distance.Split(": ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)[1];
+
+            var inputDistance = numberRegex.Matches(distanceStr).Select(match => match.Value).ToList();
+            for (int i = 0; i < inputDistance.Count; i++)
+            {
+                Number num = new Number();
+                num.Time = int.Parse(inputTime[i]);
+                num.Dist = int.Parse(inputDistance[i]);
+                numbers.Add(num);
+                Console.WriteLine(inputDistance[i]);
+
+            }
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var win = 0;
+                for (int j = 0; j < numbers[i].Time; j++)
+                {
+                    if ((numbers[i].Time - j) * j > numbers[i].Dist)
+                        win++;
+
+                }
+                total *= win;
+                Console.WriteLine(inputDistance[i]);
+
+            }
+
+            Console.WriteLine(total);
+            return total;
+        }
+    }
+}
---
+++ star7/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace star7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Star7!");
+
+            var inputData = File.ReadAllLines(@"C:\Stars\adventofcode\star7\input.txt");
+
+            var hands = new List<Hand>();
+
+            foreach (var line in inputData)
+            {
+                var data = line.Split(' ');
+                var cards = data[0].Trim().ToList();
+                var bid = long.Parse(data[1].Trim());
+
+                hands.Add(new Hand { CardsOnHand = cards, Bid = bid });
+            }
+
+            long partOne = 0;
+
+            var orderedHands = hands.OrderBy(x=>x).ToList();
+
+            for (int i = 1; i <= orderedHands.Count; i++)
+            {
+                partOne += orderedHands[i - 1].Bid * i;
+            }
+
+            Console.WriteLine($"Part two: {partOne}");
+        }
+        internal class Hand : IComparable<Hand>
+        {
+            List<char> _order = new List<char>() { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' };
+
+            public List<char> _cardsOnHand = new List<char>();
+            public List<char> CardsOnHand
+            {
+                get
+                { return _cardsOnHand; }
+                set
+                {
+                    _cardsOnHand = value;
+                }
+            }
+            public long Bid { get; set; }
+
+            public int CompareTo(Hand? other)
+            {
+                if (CardsKind < other.CardsKind)
+                {
+                    return 1;
+                }
+                else if (CardsKind > other.CardsKind)
+                {
+                    return -1;
+                }
+
+                for (int i = 0; i < CardsOnHand.Count; i++)
+                {
+                    if (_order.IndexOf(CardsOnHand[i]) < _order.IndexOf(other.CardsOnHand[i]))
+                    {
+                        return 1;
+                    }
+                    else if (_order.IndexOf(CardsOnHand[i]) > _order.IndexOf(other.CardsOnHand[i]))
+                    {
+                        return -1;
+                    }
+                }
+
+                return 0;
+            }
+
+            public CardsKind CardsKind => GetCardsKind();
+
+            private CardsKind GetCardsKind()
+            {
+                var groupedCards = CardsOnHand.GroupBy(x => x);
+
+                if (CardsOnHand.All(x => x.Equals(CardsOnHand[0])) || CardsOnHand.Contains('J') && CardsOnHand.Distinct().Count() == 2)
+                {
+                    return CardsKind.FiveOfKind;
+                }
+                else if (groupedCards.Any(group => group.Count() == 4) ||
+                         groupedCards.Any(group => group.Count() == 3 && group.Key != 'J' && CardsOnHand.Contains('J')) || 
+                         groupedCards.Any(group => group.Count() == 2 && group.Key != 'J' && CardsOnHand.Count(x => x == 'J') == 2) || 
+                         groupedCards.Any(group => group.Count() == 1 && group.Key != 'J' && CardsOnHand.Count(x => x == 'J') == 3)) 
+                {
+                    return CardsKind.FourOfKind;
+                }
+                else if (groupedCards.Any(group => group.Count() == 3) && groupedCards.Any(group => group.Count() == 2) ||
+                         (groupedCards.Where(group => group.Count() == 2).Count() == 2) && CardsOnHand.Contains('J'))
+                {
+                    return CardsKind.FullHouse;
+                }
+                else if (groupedCards.Any(group => group.Count() == 3) ||
+                         groupedCards.Any(group => group.Count() == 2) && CardsOnHand.Contains('J')) 
+                {
+                    return CardsKind.ThreeOfKind;
+                }
+                else if (groupedCards.Where(group => group.Count() == 2).Count() == 2) 
+                {
+                    return CardsKind.TwoPair;
+                }
+                else if (groupedCards.Any(group => group.Count() == 2) || CardsOnHand.Contains('J')) 
+                {
+                    return CardsKind.OnePair;
+                }
+                else
+                {
+                    return CardsKind.HighCard;
+                }
+            }
+        }
+
+        internal enum CardsKind
+        {
+            FiveOfKind,
+            FourOfKind,
+            FullHouse,
+            ThreeOfKind,
+            TwoPair,
+            OnePair,
+            HighCard
+        }
+    }
+}
